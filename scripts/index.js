987ff6fe75a2b0d6b775fc157387005c8dde5e41
HMorales <HMorales03337@gmail.com>
2021-07-21 22:05:31 -0500

    added copyright added type chart with defense type

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 var type1 = type2 = -1;
 
+var defendingType = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
 function setImg() {
     let divpkm = document.getElementById('pkm');
     let divImg = document.getElementById('divImg');
@@ -46,7 +48,33 @@ function setbgColor() {
         divImg.style.backgroundImage = `${text}${bgColors[type1]}, ${bgColors[type2]})`;
 }
 
+function multTC(t_type) {
+    for (let i = 0; i < 18; ++i)
+        defendingType[i] *= typeChar[i][t_type];
+}
+
+function getTypeChart() {
+    defendingType = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+    if( type1 == type2 ) {
+        if ( type1 != -1 && type2 != -1 )
+            multTC(type1);
+    }
+    else if( type1 == -1)
+        multTC(type2);
+    else if( type2 == -1 )
+        multTC(type1);
+    else {
+        multTC(type1);
+        multTC(type2);
+    }
+
+    for (let i = 0; i < 18; i++) {
+        document.getElementById(`a${i}`).innerHTML = defendingType[i];
+    }
+}
+
 function getType() {
     setImg();
     setbgColor();
+    getTypeChart();
 }
